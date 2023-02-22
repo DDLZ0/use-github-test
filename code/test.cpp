90f901b76a270f39c670f4d318cb518373fa24fb
lizhi <lizhi163@example.com>
2023-02-22 21:12:50 +0800

    add a ovprotect code

--- code/test.cpp
+++ code/test.cpp
@@ -1,8 +1,17 @@
 
 #include <stdio.h>
 
+void ovptotect();
+
 int main() {
 
 	printf("lizhi is beatiful\n");
 	return 0;
+}
+
+void ovptotect()
+{
+	float a = 0.0, b = 15.0;
+	float c = a + b;
+	
 }
